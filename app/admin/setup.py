17eea6b00c6c7ad1d7d0576d1a07eb809fb17406
ThUnDeR-CoDeR123 <106828462+ThUnDeR-CoDeR123@users.noreply.github.com>
2023-07-25 16:53:28 +0530

    Environ file debug done

--- app/admin/setup.py
+++ app/admin/setup.py
@@ -1,5 +1,19 @@
-from os import environ
+import environ
+import os
 
-SECRET_KEY = environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-print(SQLALCHEMY_DATABASE_URI)
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI=env('SQLALCHEMY_DATABASE_URI')
